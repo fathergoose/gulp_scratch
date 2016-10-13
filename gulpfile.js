5108eac2c9242f204de941ac3e54617f2d78e877
Al Ilseman <alilseman@AILSEMAN-MBP-CHI.local>
2016-10-13 11:50:05 -0500

    watch task is working for sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass')
 
-gulp.task('default', function() {
+const sassFiles = 'assets/styles/sass/**/*.scss'
+const cssDest = 'assets/styles/css/'
+
+gulp.task('default', () => {
 	console.log('GULP GULP GULP! Aaaahhh');
 });
+
+gulp.task('styles', () => {
+	gulp.src(sassFiles)
+        .pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest(cssDest));
+});
+
+// Ensure the sass files recompile on change
+// Assuming this says watch these files,
+// if anything changes, run `gulp styles`
+gulp.task('watch', () => {
+	gulp.watch(sassFiles, ['styles'])
+})
